7cdb79b449687aaa77be96dfa4d71c6cf35f8cf3
APapeIsName <gms72901217@gmail.com>
2025-03-25 15:46:33 +0900

    Feat : 상태 올리기

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,25 @@
+import { useState } from 'react';
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from './PostsList.module.css'
 
 function PostsList() {
+    const [ enteredBody, setEnteredBody ] = useState('');
+    const [ enteredAuthor, setEnteredAuthor ] = useState('');
+    
+    function changeBodyHandler(event) {
+        setEnteredBody(event.target.value);
+    }
+
+    function changeAuthorHandler(event) {
+        setEnteredAuthor(event.target.value);
+    }
+
     return (
         <>
-            <NewPost />
+            <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler}/>
             <ul className={classes.posts}>
-                <Post author="Maximilian" body="React.js is awesome!"/>
+                <Post author={enteredAuthor} body={enteredBody}/>
                 <Post author="Manual" body="Check out the full course!"/>
             </ul>
         </>
